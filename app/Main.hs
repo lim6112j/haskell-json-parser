fc0607aa5ba3d4ff02fbbc67875cda453a8572ab
lim6112j <lim6112j@gmail.com>
2023-11-17 00:03:10 +0900

    functor, applicative, monad

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,24 @@ data JsonValue = JsonNull | JsonInt Integer | JsonString String deriving (Show, 
 
 newtype Parser a = Parser {runParser :: String -> Maybe (String, a)}
 
+instance Functor Parser where
+  fmap f (Parser a) = Parser $ \input -> do
+    (input', x) <- a input
+    return (input', f x)
+
+instance Applicative Parser where
+  pure a = Parser $ \input -> do
+    return (input, a)
+  (Parser f) <*> (Parser a) = Parser $ \input -> do
+    (input', g) <- f input
+    (input'', x) <- a input'
+    return (input'', g x)
+
+instance Monad Parser where
+  (Parser a) >>= f = Parser $ \input -> do
+    (input', x) <- a input
+    runParser (f x) input'
+
 charP :: Char -> Parser Char
 charP x = Parser f
   where
@@ -14,5 +32,10 @@ charP x = Parser f
       | otherwise = Nothing
     f [] = Nothing
 
+stringP :: String -> Parser String
+stringP str = undefined
+
 main :: IO ()
-main = someFunc
+main = do
+  let f = runParser $ sequence $ charP <$> "hel"
+  print $ f "hello"
